757f74ddc6322ec76bac2eb459356706da5910d6
hhaydencubucaedu <hhayden@cub.uca.edu>
2025-02-14 10:45:21 -0600

    Added more detailed comments for Deadline

--- src/model/Deadline.java
+++ src/model/Deadline.java
@@ -3,29 +3,47 @@ package model;
 import java.time.LocalDateTime;
 
 /**
- * Class representing a deadline event.
- * Implements Completable so it can be marked as complete.
+ * This class represents a deadline event.
+ * It extends Event (because deadlines are events) and
+ * implements Completable (so we can mark them as done).
  */
 public class Deadline extends Event implements Completable {
-    private boolean complete;
+    private boolean complete; // This keeps track of whether the deadline is done or not.
 
+    /**
+     * Constructor for a Deadline.
+     * @param name The name of the deadline.
+     * @param deadline The date and time the deadline is due.
+     */
     public Deadline(String name, LocalDateTime deadline) {
-        super(name, deadline);
-        this.complete = false; // Initially, the deadline is not complete.
+        super(name, deadline); // Calls the constructor of the Event class.
+        this.complete = false; // Deadlines start off as incomplete (duh).
     }
 
+    /**
+     * Gets the name of the deadline.
+     * @return The name of the deadline.
+     */
     @Override
     public String getName() {
-        return name;
+        return name; // Just returning the name, nothing fancy here.
     }
 
+    /**
+     * Marks this deadline as complete.
+     * Basically flips the 'complete' switch to true.
+     */
     @Override
     public void complete() {
-        this.complete = true; // Marks the deadline as complete.
+        this.complete = true; // Boom. Now it's done.
     }
 
+    /**
+     * Checks if the deadline is complete.
+     * @return true if complete, false if not.
+     */
     @Override
     public boolean isComplete() {
-        return complete;
+        return complete; // If it's done, it's true. If not, it's false. Simple.
     }
 }
